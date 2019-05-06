00bc8b69e6ac9d5bdb8254822f3222cd397ab859
luka1baljak <luka1baljak@outlook.com>
2019-05-06 15:54:31 +0200

    Promjene
    Napravljen login i logout page, linkano... Dodan jedan user: username:susan
    password:cat

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import login
+from flask_login import UserMixin
 
 
 tablica_povezivanja=db.Table('tablica_povezivanja',
@@ -7,7 +10,7 @@ tablica_povezivanja=db.Table('tablica_povezivanja',
     db.Column('user_id',db.Integer, db.ForeignKey('user.id'))
     )
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(64), index=True, unique=True)
     email=db.Column(db.String(120), index=True, unique=True)
@@ -22,6 +25,11 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}, a moje ime je {}>'.format(self.username, self.ime)
 
+    def set_password(self,password):
+        self.password_hash=generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash, password)
 
 
 
@@ -41,5 +49,7 @@ class Izlet(db.Model):
     def __repr__(self):
         return '<Izlet {}>'.format(self.naziv)
 
-
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
